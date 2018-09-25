b49af176d70fefc54b5050dfe5304bf85963a23d
trakieca <emil_vaklinov@yahoo.com>
2018-09-25 14:45:44 +0100

    Charts added and working

--- client/src/views/chart_view.js
+++ client/src/views/chart_view.js
@@ -7,17 +7,17 @@ const ViewChart = function(myChart_x){
 
 ViewChart.prototype.renderChart = function(testChartData, chartTitle){
   this.myChart_x.setOption({
-      title: {
-          text: chartTitle
-      },
-      tooltip: {},
-      series: [{
-          radius: '50%',
-          type: 'pie',
-          data:[
-                  {value:testChartData[0], name:testChartData[0]},
-                  {value:testChartData[1], name:testChartData[1]},
-                ]
+    title: {
+      text: chartTitle
+    },
+    tooltip: {},
+    series: [{
+      radius: '50%',
+      type: 'pie',
+      data:[
+        {value:testChartData[0], name:testChartData[0]},
+        {value:testChartData[1], name:testChartData[1]},
+      ]
     }]
   });
 }
