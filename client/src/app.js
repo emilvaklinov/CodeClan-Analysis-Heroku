037219030dcaa2d49a2af115763ea90fa189bd94
trakieca <emil_vaklinov@yahoo.com>
2018-09-25 12:28:57 +0100

    little cleaner chart

--- client/src/app.js
+++ client/src/app.js
@@ -3,6 +3,7 @@ const ListView = require('./views/list_view');
 const Searches = require('./models/searches');
 const MapView = require('./views/map_view');
 const ChartView = require('./views/chart_view.js');
+const echarts = require('echarts');
 
 document.addEventListener('DOMContentLoaded', () => {
   let url = 'http://localhost:3000/api/inputs';
@@ -18,12 +19,25 @@ document.addEventListener('DOMContentLoaded', () => {
   const mapView = new MapView();
 
 
-  const chartView = new ChartView();
+  const myChart_1 = echarts.init(document.getElementById('chart_1'));
+  const chartView_1 = new ChartView(myChart_1);
+  let testChartData_1 = [20, 100];
+  let chartTitle_1 = "retweets"
+  chartView_1.renderChart(testChartData_1, chartTitle_1);
+
+
+
+  const myChart_2 = echarts.init(document.getElementById('chart_2'));
+  const chartView_2 = new ChartView(myChart_2);
+  let testChartData_2 = [80, 150];
+  let chartTitle_2 = "likes"
+  chartView_2.renderChart(testChartData_2, chartTitle_2);
+
 
 
   formView.bindEvents();
   listView.bindEvents();
   searches.bindEvents();
   mapView.renderMap();
-  chartView.renderChart();
+
 })
--- client/src/views/chart_view.js
+++ client/src/views/chart_view.js
@@ -1,29 +1,32 @@
-var echarts = require('echarts');
+// var echarts = require('echarts');
 
-const ViewChart = function(){}
+const ViewChart = function(myChart_x){
+  this.myChart_x = myChart_x;
+}
 
-const testChartData = [20, 100];
+// ViewChart.prototype.renderAllCharts = function(){
+//   this.renderChart([20, 100],'retweets');
+//   this.renderChart([25,90],'likes');
+//   this.renderChart([30,150],'followers');
+// }
 
 
-ViewChart.prototype.renderChart = function(){
-// initialize echarts instance with prepared DOM
-var myChart = echarts.init(document.getElementById('chart_1'));
-// draw chart
-myChart.setOption({
-    title: {
-        text: 'by Retweets'
-    },
-    tooltip: {},
-    series: [{
-        // name: 'no of retweets',
-        radius: '55%',
-        type: 'pie',
-        data:[
-                {value:testChartData[0], name:testChartData[0]},
-                {value:testChartData[1], name:testChartData[1]},
-              ]
-  }]
-});
+
+ViewChart.prototype.renderChart = function(testChartData, chartTitle){
+  this.myChart_x.setOption({
+      title: {
+          text: chartTitle
+      },
+      tooltip: {},
+      series: [{
+          radius: '50%',
+          type: 'pie',
+          data:[
+                  {value:testChartData[0], name:testChartData[0]},
+                  {value:testChartData[1], name:testChartData[1]},
+                ]
+    }]
+  });
 }
 
 module.exports = ViewChart;
