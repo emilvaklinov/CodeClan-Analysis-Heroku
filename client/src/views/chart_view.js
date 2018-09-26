037bd61c326134c64c51350962d0a3e124c5cddc
Marcin Jerwan <m.jerwan@gmail.com>
2018-09-26 10:42:31 +0100

    titles of charts into charts

--- client/src/views/chart_view.js
+++ client/src/views/chart_view.js
@@ -10,11 +10,10 @@ ViewChart.prototype.bindEvents = function () {
   PubSub.subscribe('Searches:totals-calulated', (event) => {
     this.myChart_1 = echarts.init(document.getElementById('chart_1'))
     this.myChart_2 = echarts.init(document.getElementById('chart_2'))
-    this.renderChart(event.detail.retweets, event.detail.retweets, 'retweets', this.myChart_1);
-    this.renderChart(event.detail.favourites, event.detail.favourites, 'favourites', this.myChart_2);
+    this.renderChart(event.detail.retweets, event.detail.retweets, 'Positive vs Negative Retweets', this.myChart_1);
+    this.renderChart(event.detail.favourites, event.detail.favourites, 'Positive vs Negative Likes', this.myChart_2);
   })
 }
-
 
 ViewChart.prototype.renderChart = function (chartData_positive, chartData_negative, title, chart_x) {
   chart_x.setOption({
