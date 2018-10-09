29e7a733d61ad0382fcd6abd199fc63dcbf87b59
trakieca <emil_vaklinov@yahoo.com>
2018-10-09 15:21:52 +0100

    heroku updates

--- server/server.js
+++ server/server.js
@@ -46,6 +46,6 @@ app.get('/api/geocoder-results', function (req, res) {
     });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log(`Listening on port ${this.address().port}`);
 })
